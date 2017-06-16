7dfa7766a6140a7319424b6442884fba3ad6df9c
Ben Formosa <ben.formosa@gmail.com>
2017-06-16 20:12:10 +1000

    Update tests

--- test/test-context.sh
+++ test/test-context.sh
@@ -4,6 +4,11 @@
 set -eo pipefail
 
 SCRIPT="$(dirname $0)/../docker/context.pl"
+DIFF="git diff --no-index --color-words=."
+DODIFF=1 # Set to any value to $DIFF against static values
+
+export CONTEXT="app"
+export SECRETS_DIR="$(dirname $0)"
 
 function unset_vars {
     unset MYDB_HOST
@@ -12,19 +17,21 @@ function unset_vars {
     unset MYDB_RESOURCE
     unset MYDB_URL
     unset MYDB_USER
+    unset PARAM_FOO_NAME
+    unset PARAM_FOO_VALUE
+    unset PARAM_FOO_OVERRIDE
 }
 
-export CONTEXT="app"
-export SECRETS_DIR="$(dirname $0)"
+function test1 {
+    echo "Test: Constructed JDBC URL - default driver"
+    export MYDB_HOST="database.example.com"
+    export MYDB_NAME="MYDB"
+    export MYDB_PORT="1521"
+    export MYDB_RESOURCE="MYDB"
+    export MYDB_USER="DB-Admin"
 
-echo "Test: Constructed JDBC URL - default driver"
-export MYDB_HOST="database.example.com"
-export MYDB_NAME="MYDB"
-export MYDB_PORT="1521"
-export MYDB_RESOURCE="MYDB"
-export MYDB_USER="DB-Admin"
-
-diff <($SCRIPT) <(cat <<EOF
+    if [ "$DODIFF" ]; then
+        $DIFF <($SCRIPT) <(cat <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <Context path="/app">
     <Resource name="jdbc/MYDB"
@@ -42,23 +49,28 @@ diff <($SCRIPT) <(cat <<EOF
 </Context>
 EOF
 )
+        if [ $? -eq 0 ]; then
+            echo "test OK"
+        else
+            echo "test failed"
+        fi
+    else
+        $SCRIPT
+    fi
+    unset_vars
+}
 
-if [ $? -eq 0 ]; then
-    echo "test OK"
-else
-    echo "test failed"
-fi
-unset_vars
+function test2 {
+    echo "Test: Constructed JDBC URL"
+    export MYDB_DRIVER="mssql"
+    export MYDB_HOST="database.example.com"
+    export MYDB_NAME="MYDB"
+    export MYDB_PORT="1521"
+    export MYDB_RESOURCE="MYDB"
+    export MYDB_USER="DB-Admin"
 
-echo "Test: Constructed JDBC URL"
-export MYDB_DRIVER="mssql"
-export MYDB_HOST="database.example.com"
-export MYDB_NAME="MYDB"
-export MYDB_PORT="1521"
-export MYDB_RESOURCE="MYDB"
-export MYDB_USER="DB-Admin"
-
-diff <($SCRIPT) <(cat <<EOF
+    if [ "$DODIFF" ]; then
+        $DIFF <($SCRIPT) <(cat <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <Context path="/app">
     <Resource name="jdbc/MYDB"
@@ -76,21 +88,26 @@ diff <($SCRIPT) <(cat <<EOF
 </Context>
 EOF
 )
+        if [ $? -eq 0 ]; then
+            echo "test OK"
+        else
+            echo "test failed"
+        fi
+    else
+        $SCRIPT
+    fi
+    unset_vars
+}
 
-if [ $? -eq 0 ]; then
-    echo "test OK"
-else
-    echo "test failed"
-fi
-unset_vars
+function test3 {
+    echo "Test: Specific JDBC URL"
+    export MYDB_DRIVER="mssql"
+    export MYDB_RESOURCE="MYDB"
+    export MYDB_URL="jdbc:oracle:thin:@(DESCRIPTION=(FAILOVER=ON)(ADDRESS=(PROTOCOL=TCP)(HOST=db01.example.com)(PORT=1521))(ADDRESS=(PROTOCOL=TCP)(HOST=db02.example.com)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=INSTANCE.example.com)))"
+    export MYDB_USER="DB-Admin"
 
-echo "Test: Specific JDBC URL"
-export MYDB_DRIVER="mssql"
-export MYDB_RESOURCE="MYDB"
-export MYDB_URL="jdbc:oracle:thin:@(DESCRIPTION=(FAILOVER=ON)(ADDRESS=(PROTOCOL=TCP)(HOST=db01.example.com)(PORT=1521))(ADDRESS=(PROTOCOL=TCP)(HOST=db02.example.com)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=INSTANCE.example.com)))"
-export MYDB_USER="DB-Admin"
-
-diff <($SCRIPT) <(cat <<EOF
+    if [ "$DODIFF" ]; then
+        $DIFF <($SCRIPT) <(cat <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <Context path="/app">
     <Resource name="jdbc/MYDB"
@@ -108,11 +125,63 @@ diff <($SCRIPT) <(cat <<EOF
 </Context>
 EOF
 )
+        if [ $? -eq 0 ]; then
+            echo "test OK"
+        else
+            echo "test failed"
+        fi
+    else
+        $SCRIPT
+    fi
+    unset_vars
+}
 
-if [ $? -eq 0 ]; then
-    echo "test OK"
-else
-    echo "test failed"
-fi
-unset_vars
+function test4 {
+    echo "Test: Parameters"
+    export MYDB_HOST="database.example.com"
+    export MYDB_NAME="MYDB"
+    export MYDB_PORT="1521"
+    export MYDB_RESOURCE="MYDB"
+    export MYDB_USER="DB-Admin"
+    export PARAM_FOO_NAME="foo"
+    export PARAM_FOO_VALUE="bar"
+    export PARAM_FOO_OVERRIDE="false"
 
+    if [ "$DODIFF" ]; then
+        $DIFF <($SCRIPT) <(cat <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<Context path="/app">
+    <Resource name="jdbc/MYDB"
+        auth="Container"
+        type="javax.sql.DataSource"
+        maxActive="10"
+        maxIdle="2"
+        maxWait="2000"
+        username="DB-Admin"
+        password="pass"
+        driverClassName="oracle.jdbc.OracleDriver"
+        url="jdbc:oracle:thin:@database.example.com:1521:MYDB"
+        validationQuery="select 1 from dual"
+    />
+    <Parameter name="foo"
+        value="bar"
+        override="false"
+    />
+</Context>
+EOF
+)
+        if [ $? -eq 0 ]; then
+            echo "test OK"
+        else
+            echo "test failed"
+        fi
+    else
+        $SCRIPT
+    fi
+    unset_vars
+}
+
+test1
+test2
+test3
+test4
